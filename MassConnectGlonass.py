a1d21ae237f54e37af5e3654460772ae3fb582ee
Denvus <denis.shalimov98@gmail.com>
2025-10-29 15:22:08 +0300

    get carrier id

--- MassConnectGlonass.py
+++ MassConnectGlonass.py
@@ -13,13 +13,21 @@ class MassConnect:
         self.refresh_token = self.get_access_token(login, password).json()['refresh_token']
         self.brands = self.getBrands()
             
-    def set_object_monitoring(self, status, brandName, modelName, trackerNumber, trackerPhoneNumber, startDate, endDate, vehicleName):
-        response_get_vehicles_info = self.getVehiclesInfo(vehicleName)
+    def set_object_monitoring(self, status, brandName, modelName, trackerNumber, trackerPhoneNumber, startDate, endDate, vehicleName, carrier):
+        first_response = self.getVehiclesInfo(vehicleName, "")
+        # если ТС с определенным номером найден один
+        if len(first_response) == 1:
+            response_get_vehicles_info = first_response
+        else:
+            #если ТС хуева гора, Уточняем кто ГП и отправляем запрос заново
+            response_get_vehicles_info = self.getVehiclesInfo(vehicleName, "&carrier=" + str(self.get_id_carrier(carrier)))
         if len(response_get_vehicles_info) != 1:
-            print(f'ТС с номером {vehicleName} найдено {len(response_get_vehicles_info)}')
+            print (f"для Тс {vehicleName} {carrier} найдено {len(response_get_vehicles_info)}")
         else:
             vehicleId = response_get_vehicles_info[0]['id']
             carrierId = response_get_vehicles_info[0]['carrier']['id']
+
+            
         
             access_token: str = self.refreshToken(self.refresh_token)
             url = f'https://{environment}/api/trackers/v1/trackers'
@@ -42,10 +50,10 @@ class MassConnect:
             try:
                 res = requests.post(url, headers=headers, data=json.dumps(data))
                 if res.status_code != 201:
-                    print(f"Ошибка при отправке запроса. v.id = {vehicleId}, Код {res.status_code}. Ошибка: {res.text}" )
-                print(f"{res.status_code}, v.id = {vehicleId}")
+                    print(f"Не успешно, {vehicleName} {carrier}. Ошибка при отправке запроса. Код {res.status_code}. Ошибка: {res.text}" )
+                print(f"Успешно, {vehicleName} {carrier}")
             except:
-                print(f"Ошибка отправки запроса. v.id = {vehicleId}")
+                print(f"Не успешно, {vehicleName} {carrier}. Ошибка отправки запроса. ")
 
     def set_object_monitoring_by_id(self, status, brandName, modelName, trackerNumber, trackerPhoneNumber, startDate, endDate, vehicleId, carrierId):
             #time.sleep(random.randint(1,10))
@@ -58,8 +66,8 @@ class MassConnect:
             data = {
                 "provider": "glonass",
                 "status": self.getStatus(status),
-                "brandId": self.brands[brandName],
-                "modelId": massConnect.getModels(self.brands[brandName], modelName),
+                "brandId": None, # self.brands[brandName],
+                "modelId": None, # massConnect.getModels(self.brands[brandName], modelName),
                 "trackerNumber": trackerNumber,
                 "trackerPhoneNumber": trackerPhoneNumber,
                 "startDate": startDate,
@@ -148,9 +156,9 @@ class MassConnect:
                 return (item['id'])
         print (f"Нет модели трекера {model_name} для выбранного трекера.")
 
-    def getVehiclesInfo(self, vehicle_number):
+    def getVehiclesInfo(self, vehicle_number, carrier):
         licence_plate = quote("*" + vehicle_number + "*")
-        url = f'https://{environment}/api/native/v1.0/vehicles?page=1&per-page=20&order-by=id%7Cdesc&licensePlate={licence_plate}'
+        url = f'https://{environment}/api/native/v1.0/vehicles?page=1&per-page=20&order-by=id%7Cdesc&licensePlate={licence_plate}{carrier}'
         access_token = self.refreshToken(self.refresh_token)
         headers = {
             'Content-Type': 'application/json',
@@ -158,14 +166,22 @@ class MassConnect:
         }
         return requests.get(url, headers=headers).json()['entities']
         
-        
+    def get_id_carrier(self, carrier):
+        access_token = self.refreshToken(self.refresh_token)
+
+        url = f'https://{environment}/api/native/v1.0/carriers?per-page=100&page=1&status%5B%5D=confirmed&status%5B%5D=not_confirmed&status%5B%5D=trial&status%5B%5D=confirmation_process&status%5B%5D=deleted&status%5B%5D=rejected&status%5B%5D=confirmation_process_with_eq&profileDetails%5BorganizationName%5D='
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {access_token}'
+        }
+        response = requests.get(url + quote("*" + carrier + "*", encoding='utf-8'), headers=headers).json()
+        if(response["total"] == 1):
+            return response["entities"][0]["id"]
+        else:
+            return ""
+
+
     
 if __name__ == '__main__':
-    massConnect = MassConnect('Login', 'Password')
-
-
-    # пример
-    massConnect.set_object_monitoring("Удален","RETRANSLATOR","Wialon IPS","000000000","",None,None,"А000АА000")
-  
-
-
+    massConnect = MassConnect('login', 'password')
+   
